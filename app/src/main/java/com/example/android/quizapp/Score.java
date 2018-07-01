00ccca8aa6fa3d44948c31f945bb4f16e9b1fc5a
ZephyrVic <onyeneke.victor9@gmail.com>
2018-07-01 21:19:50 +0100

    updated the Score Page with Buttons the restart Quiz and send quiz score as email

--- app/src/main/java/com/example/android/quizapp/Score.java
+++ app/src/main/java/com/example/android/quizapp/Score.java
@@ -1,10 +1,16 @@
 package com.example.android.quizapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Score extends AppCompatActivity {
+
+    String Score;
+    String Profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +22,29 @@ public class Score extends AppCompatActivity {
 
     public void displayProfile(){
 
-        String Score = getIntent().getStringExtra("Score");
-        String Profile = getIntent().getStringExtra("profile");
+        Score = getIntent().getStringExtra("Score");
+        Profile = getIntent().getStringExtra("profile");
 
         TextView profileText = (TextView) findViewById(R.id.profile);
         profileText.setText(Profile);
         TextView scoreDisplay = (TextView) findViewById(R.id.finalScore);
         scoreDisplay.setText(Score);
 
+    }
+    public void composeEmail (View view){
+        String mailBody = Profile + "\n\n";
+        mailBody += "I Scored " + Score + " in the Power Sector Trivia";
+
+        Intent sendEmail = new Intent(Intent.ACTION_SENDTO);
+        sendEmail.setData(Uri.parse("mailto:"));
+        sendEmail.putExtra(Intent.EXTRA_SUBJECT, "MY POWER SECTOR TRIVIA SCORE");
+        sendEmail.putExtra(Intent.EXTRA_TEXT, mailBody);
+        if (sendEmail.resolveActivity(getPackageManager()) != null){
+            startActivity(sendEmail);
+        }
+    }
+    public void restartQuiz (View view){
+        Intent profilePage = new Intent(this, ProfileActivity.class);
+        startActivity(profilePage);
     }
 }
